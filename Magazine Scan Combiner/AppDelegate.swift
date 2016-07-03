242291ae14c632f0ab8742ef989c359a02dbabff
Prachi Gauriar <prachi@ticketmaster.com>
2016-07-03 15:20:12 -0400

    Initial Commit

---
+++ Magazine Scan Combiner/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Magazine Scan Combiner
+//
+//  Created by Prachi Gauriar on 7/3/2016.
+//  Copyright © 2016 Quantum Lens Cap. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
